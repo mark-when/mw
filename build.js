5c67a51715b73acd8097c10c661dfd7bb9fcdbe2
Rob Koch <kochrt@gmail.com>
2023-04-04 13:17:33 -0400

    Change html -> view-templates

--- build.js
+++ build.js
@@ -1,5 +1,14 @@
 import { cpSync } from "fs";
 
-cpSync('node_modules/@markwhen/timeline/dist/index.html', 'html/timeline.html')
-cpSync('node_modules/@markwhen/resume/dist/index.html', 'html/resume.html')
-cpSync('node_modules/@markwhen/calendar/dist/index.html', 'html/calendar.html')
+cpSync(
+  "node_modules/@markwhen/timeline/dist/index.html",
+  "view-templates/timeline.html"
+);
+cpSync(
+  "node_modules/@markwhen/resume/dist/index.html",
+  "view-templates/resume.html"
+);
+cpSync(
+  "node_modules/@markwhen/calendar/dist/index.html",
+  "view-templates/calendar.html"
+);
